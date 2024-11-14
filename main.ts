a445c3b2ae174d9884231610cfe2088ecc19d938
Ben Floyd <ben.floyd@simplot.com>
2024-11-13 17:37:54 -0700

    Reforming

--- main.ts
+++ main.ts
@@ -1,81 +1,94 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, ItemView, WorkspaceLeaf } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
 interface VTSSettings {
-	mySetting: string;
+	youtubeApiKey: string;
 }
 
 const DEFAULT_SETTINGS: VTSSettings = {
-	mySetting: 'default'
+	youtubeApiKey: ''
+}
+
+export const VIEW_TYPE_TRANSCRIPTION = "transcription-view";
+
+export class TranscriptionView extends ItemView {
+	content: string;
+
+	constructor(leaf: WorkspaceLeaf) {
+		super(leaf);
+		this.content = '';
+	}
+
+	getViewType() {
+		return VIEW_TYPE_TRANSCRIPTION;
+	}
+
+	getDisplayText() {
+		return "Video Transcription";
+	}
+
+	setContent(content: string) {
+		this.content = content;
+		this.refresh();
+	}
+
+	async onOpen() {
+		this.refresh();
+	}
+
+	async refresh() {
+		const container = this.containerEl.children[1];
+		container.empty();
+		container.createEl("div", { text: this.content });
+	}
 }
 
 export default class VTS extends Plugin {
 	settings: VTSSettings;
+	view: TranscriptionView;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.registerView(
+			VIEW_TYPE_TRANSCRIPTION,
+			(leaf) => new TranscriptionView(leaf)
+		);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.addCommand({
+			id: 'get-video-transcription',
+			name: 'Get Video Transcription',
+			editorCallback: async (editor: Editor) => {
+				const selection = editor.getSelection();
+				if (!selection) {
+					new Notice('No URL selected');
+					return;
+				}
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new VTSModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new VTSModal(this.app).open();
-					}
+				if (!this.isYouTubeUrl(selection)) {
+					new Notice('Invalid YouTube URL');
+					return;
+				}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				const videoId = this.extractVideoId(selection);
+				if (!videoId) {
+					new Notice('Could not extract video ID');
+					return;
+				}
+
+				try {
+					const transcript = await this.getYouTubeTranscript(videoId);
+					const view = await this.activateView();
+					view.setContent(transcript);
+				} catch (error) {
+					new Notice('Failed to fetch transcript');
+					console.error(error);
 				}
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new VTSSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -88,6 +101,41 @@ export default class VTS extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+	}
+
+	async activateView() {
+		const { workspace } = this.app;
+		
+		let leaf = workspace.getRightLeaf(false);
+		if (!leaf) {
+			leaf = workspace.getLeaf('split', 'vertical');
+		}
+		
+		await leaf.setViewState({
+			type: VIEW_TYPE_TRANSCRIPTION,
+			active: true,
+		});
+
+		this.view = leaf.view as TranscriptionView;
+		return this.view;
+	}
+
+	async getYouTubeTranscript(videoId: string): Promise<string> {
+		// This is where you'd implement the YouTube API call
+		// You'll need to use the YouTube Data API or a third-party library
+		// For now, returning a placeholder
+		return `Transcript for video ${videoId}`;
+	}
+
+	isYouTubeUrl(url: string): boolean {
+		const pattern = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/.+$/;
+		return pattern.test(url);
+	}
+
+	extractVideoId(url: string): string | null {
+		const regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+		const match = url.match(regExp);
+		return (match && match[2].length === 11) ? match[2] : null;
 	}
 }
 
@@ -117,17 +165,16 @@ class VTSSettingTab extends PluginSettingTab {
 
 	display(): void {
 		const {containerEl} = this;
-
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('YouTube API Key')
+			.setDesc('Enter your YouTube Data API key')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter API key')
+				.setValue(this.plugin.settings.youtubeApiKey)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.youtubeApiKey = value;
 					await this.plugin.saveSettings();
 				}));
 	}
